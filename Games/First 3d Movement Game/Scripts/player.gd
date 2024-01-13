9f8e8c03fbdb41364736353f3c6e131798e7bf57
Aiden J <junga526@hsd.k12.or.us>
2024-01-13 14:15:39 -0800

    added extra parkour and ramp testing

--- Games/First 3d Movement Game/Scripts/player.gd
+++ Games/First 3d Movement Game/Scripts/player.gd
@@ -119,6 +119,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
+
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity

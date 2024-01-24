df7c62e7d3bff5bbc4e842a447423f6ff8dffa73
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-01-24 15:20:23 -0800

    started working on a checkpoint system

---
+++ Games/First 3d Movement Game/Scripts/Checkpoint.gd
@@ -0,0 +1,9 @@
+extends Area3D
+
+@export var spawnpoint = false
+
+var activated = false
+
+
+func _on_area_entered(_area):
+		print("checkpoint")
--- Games/First 3d Movement Game/Scripts/player.gd
+++ Games/First 3d Movement Game/Scripts/player.gd
@@ -1,5 +1,5 @@
 extends CharacterBody3D
-
+class_name Player
 # Player Nodes
 @onready var neck = $Neck
 @onready var head = $Neck/Head

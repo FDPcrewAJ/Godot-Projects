41b5f6870a2e2eafb49aaf11c6cdb7fcf685bdbf
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-01-30 15:30:12 -0800

    thought I made progress on checkpoints but ig not

--- Games/First 3d Movement Game/Scripts/Checkpoint.gd
+++ Games/First 3d Movement Game/Scripts/Checkpoint.gd
@@ -1,5 +1,10 @@
 extends Area3D
 
-func _on_area_entered(body):
+func save_check_point(pos_x, pos_y, pos_z):
+	global.check_point_pos.x = pos_x
+	global.check_point_pos.y = pos_y
+	global.check_point_pos.z = pos_z
+
+func _on_area_entered(_body):
 	print("checkpoint")
-	body.save_check_point(self.position.x, self.position.y, self.position.z)
+	save_check_point(self.position.x, self.position.y, self.position.z)
